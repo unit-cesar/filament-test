684fb8bc328ddf9ac57b92329229c261e9f778e1
unit-cesar <unit.cesar@gmail.com>
2025-04-11 12:28:35 -0300

    feat: Admin panel instaled

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,13 +2,13 @@
 
 return [
     App\Providers\AppServiceProvider::class,
-    Filament\Support\SupportServiceProvider::class,
-    Filament\Support\SupportServiceProvider::class,
+    App\Providers\Filament\AdminPanelProvider::class,
     Filament\Actions\ActionsServiceProvider::class,
     Filament\FilamentServiceProvider::class,
     Filament\Forms\FormsServiceProvider::class,
     Filament\Infolists\InfolistsServiceProvider::class,
     Filament\Notifications\NotificationsServiceProvider::class,
+    Filament\Support\SupportServiceProvider::class,
     Filament\Tables\TablesServiceProvider::class,
-    Filament\Widgets\WidgetsServiceProvider::class
+    Filament\Widgets\WidgetsServiceProvider::class,
 ];
