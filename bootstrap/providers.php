664ee1b2d2e681fedbde549c99391f774b05941b
unit-cesar <unit.cesar@gmail.com>
2025-04-11 11:43:29 -0300

    build: Instaled Filament and all resources

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,4 +2,13 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    Filament\Support\SupportServiceProvider::class,
+    Filament\Support\SupportServiceProvider::class,
+    Filament\Actions\ActionsServiceProvider::class,
+    Filament\FilamentServiceProvider::class,
+    Filament\Forms\FormsServiceProvider::class,
+    Filament\Infolists\InfolistsServiceProvider::class,
+    Filament\Notifications\NotificationsServiceProvider::class,
+    Filament\Tables\TablesServiceProvider::class,
+    Filament\Widgets\WidgetsServiceProvider::class
 ];
